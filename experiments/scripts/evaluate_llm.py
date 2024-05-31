486ff24d489182d9afdf76c1a0b86d74ad8d075d
djaniak <denis.janiak7@gmail.com>
2024-05-31 16:51:53 +0200

    llm eval and new notebook to validate quantization

---
+++ experiments/scripts/evaluate_llm.py
@@ -0,0 +1,37 @@
+import os
+from pathlib import Path
+from typing import Optional
+
+import torch
+import typer
+
+
+def main(
+    model_name: str = typer.Option(...),
+    task: str = typer.Option(...),
+    compression: str = typer.Option(...),
+    output_path: Optional[Path] = typer.Option(None),
+    batch_size: int = typer.Option(256),
+):
+    if output_path is None:
+        output_path = Path(f"data/lm_eval/{model_name}-{compression}-{task}.json")
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model_name = model_name.replace("_", "/")
+
+    model_args = f"pretrained={model_name}"
+    if compression == "quantization_8bit":
+        model_args += ",load_in_8bit=True"
+    elif compression == "quantization_4bit":
+        model_args += ",load_in_4bit=True"
+    else:
+        pass
+
+    os.system(
+        f"lm_eval --model hf --model_args {model_args} --tasks {task} --device {device} --output_path {output_path} --batch_size {batch_size}"
+    )
+
+
+if __name__ == "__main__":
+    typer.run(main)
